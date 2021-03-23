71c04fa91b488d20ae14df5fd6dd597c4e7cab64
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2021-03-23 12:53:41 +0000

    Add project files.

---
+++ UnitTestdemo.MyProject.Tests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnitTestingdemo.MyProject; // note we must reference the project we are testing
+
+namespace UnitTestdemo.MyProject.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        Calculator c = new Calculator();
+
+
+        [Test]
+        public void Add()
+        {
+            Assert.AreEqual(20, c.multiply(5, 4));
+        }
+
+        [Test]
+        public void div()
+        {
+            Assert.AreEqual(true, c.divisible(3, 1, 3));
+            Assert.AreEqual(true, c.divisible(12, 2, 6));
+            Assert.AreEqual(false, c.divisible(100, 5, 3));
+            Assert.AreEqual(false, c.divisible(12, 5, 3));
+
+        }
+
+        [Test]
+        public void watermelonsDiv()
+        {
+            Assert.AreEqual(true, c.Q2Div(2, 6));
+            Assert.AreEqual(true, c.Q2Div(4, 4));
+            Assert.AreEqual(false, c.Q2Div(3, 5));
+            Assert.AreEqual(false, c.Q2Div(7, 1));
+        }
+
+    }
+}
+
---
+++ UnitTestingdemo/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingdemo.MyProject
+{
+    public class Calculator
+    {
+
+        /* public int Add(int a, int b)
+         {
+
+             return a + b;
+         } */
+
+        public int multiply(int a, int b)
+        {
+            // throw new System.NotImplementedException();
+            return a * b;
+        }
+        public bool divisible(long n, long x, long y)
+        {
+            if (n % x == 0 && n % y == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Q2Div(int weight1, int weight2)
+        {
+            int evenNum = 2;
+            if (weight1 % evenNum == 0 && weight2 % evenNum == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
